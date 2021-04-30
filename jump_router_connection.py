5c4569f317f1da04aa57ce51911949686a90a79e
JohnFu11er <62799830+JohnFu11er@users.noreply.github.com>
2021-04-29 20:39:45 -0400

    Create jump_router_connection.py

---
+++ jump_router_connection.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from netmiko import ConnectHandler
+import time
+
+device = {
+    'device_type': 'cisco_ios',
+    'host': '192.168.10.1',      # this is the IP for my "jump router"
+    'username': 'bob',             # ssh username for "jump router"
+    'password': 'supersecretpassword'          # ssh password for "jump router"
+}
+
+# Initalize Netmiko object
+net_connect = ConnectHandler(**device)
+
+# Write the SSH command to the "jump router" to get to the target router
+net_connect.write_channel("ssh -l bob 192.168.1.2\n")   
+
+# Paues to give "jump router" time to respond and prompt for ssh password
+time.sleep(2)
+
+# Send ssh password for the target router
+net_connect.write_channel("supersecretpassword\n")
+
+# Print the prompt that of the router that Netmiko is actively connected to
+print(net_connect.find_prompt())    # Should print the name of your target server
